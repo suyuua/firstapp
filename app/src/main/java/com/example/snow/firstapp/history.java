008145c0b799554ed29e8642fabbb292da9f169c
suyuua <suyu233615@hotmail.com>
2017-08-14 09:56:56 -0600

    added history class and history xml

---
+++ app/src/main/java/com/example/snow/firstapp/history.java
@@ -0,0 +1,110 @@
+package com.example.snow.firstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * Created by Snow on 10-Aug-2017.
+ */
+
+public class history extends AppCompatActivity{
+
+    private Button h1,h2,h3,h4,h5,h6,h7,h8;
+    private Button historyButtons[];
+    private TextView answer, question;
+    SQLiteHelper myDb;
+
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.history);
+            init();
+            setHistory();
+            action();
+        }
+
+    private void action() {
+        h1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                question.setText("meow");
+                finish();
+
+            }
+        });
+    }
+
+
+    public void init(){
+        h1 = (Button) findViewById(R.id.h1);
+        h2 = (Button) findViewById(R.id.h2);
+        h3 = (Button) findViewById(R.id.h3);
+        h4 = (Button) findViewById(R.id.h4);
+        h5 = (Button) findViewById(R.id.h5);
+        h6 = (Button) findViewById(R.id.h6);
+        h7 = (Button) findViewById(R.id.h7);
+        h8 = (Button) findViewById(R.id.h8);
+
+        historyButtons = new Button[]{h1,h2,h3,h4,h5,h6,h7,h8};
+        answer = (TextView) findViewById(R.id.answer);
+        question = (TextView) findViewById(R.id.question);
+
+    }
+
+    public void setHistory(){
+
+        myDb = new SQLiteHelper(this);
+        Cursor res=myDb.getallQuestions();
+        if (res.getCount() == 0) return;
+       // int i=0;
+       // while (res.moveToNext()){
+       //     historyButtons[i].setText(res.getString(1)+"\n"+res.getString(2));
+        //    i++;
+        //}
+        res.moveToLast();
+        int i= 0;
+        do{
+            historyButtons[i].setText(res.getString(1)+"\n"+res.getString(2));
+            i++;
+        }
+        while(res.moveToPrevious());
+
+
+    }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.history_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.history:
+                    finish();
+                    return true;
+
+                default:
+                    // If we got here, the user's action was not recognized.
+                    // Invoke the superclass to handle it.
+                    return super.onOptionsItemSelected(item);
+
+
+            }
+        }
+
+
+}
